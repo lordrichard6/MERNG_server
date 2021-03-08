0c385b6b5dacae7dc3a5b70120a42171d6acdecd
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-08 18:13:21 +0100

    destructur register input args

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,11 +1,16 @@
-const User = require('../../models/User')
+const User = require("../../models/User");
 
 module.exports = {
-    Mutation: {
-        register(_, args, context, info){
-            // TODO: Validate user data
-            // TODO: Make sure user doesnt already exist
-            // TODO: hash password and create an auth token
-        }
-    }
-}
+  Mutation: {
+    register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
+      // TODO: Validate user data
+      // TODO: Make sure user doesnt already exist
+      // TODO: hash password and create an auth token
+    },
+  },
+};
