3effb62a528b5c769a7825ae45c490ec48c0bfe9
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-08 20:42:00 +0100

    create new user with params and encryption

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,8 +1,12 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
 module.exports = {
   Mutation: {
-    register(
+    async register(
       _,
       { registerInput: { username, email, password, confirmPassword } },
       context,
@@ -11,6 +15,31 @@ module.exports = {
       // TODO: Validate user data
       // TODO: Make sure user doesnt already exist
       // TODO: hash password and create an auth token
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
     },
   },
 };
