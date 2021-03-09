12ccbde4d2521335bf72da945a2fea23199411f4
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-09 15:36:16 +0100

    login authentication

--- util/validators.js
+++ util/validators.js
@@ -27,3 +27,18 @@ module.exports.validateRegisterInput = (
     valid: Object.keys(errors).length < 1,
   };
 };
+
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "You must type a username";
+  }
+  if (password.trim() === "") {
+    errors.password = "Please type your password";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
