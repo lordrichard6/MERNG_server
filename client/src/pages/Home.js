21d98ba09696399fdb0d886da5de2d49ba1f4c73
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-13 10:32:12 +0100

    fetch posts from db

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
 function Home() {
+  const {
+    loading,
+    data: { getPosts: post },
+  } = useQuery(FETCH_POSTS_QUERY);
+
+  if (data) {
+    console.log(data);
+  }
+
   return (
     <div>
       <h1>Home page</h1>
     </div>
   );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
 
 export default Home;
