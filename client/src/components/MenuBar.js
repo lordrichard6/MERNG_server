8aeaaa34093c2fc1873facf8061091dd2b4862e5
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-13 11:40:30 +0100

    add like button

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -11,7 +11,7 @@ function MenuBar() {
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
-    <Menu pointing secondary size="massive" color="teal">
+    <Menu pointing secondary size="massive" color="orange">
       <Menu.Item
         name="home"
         active={activeItem === "home"}
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,26 +1,37 @@
 import React from "react";
-import { Card, Icon, Label, Image } from "semantic-ui-react";
+import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import moment from "moment";
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
 }) {
+    function likePost(){
+        console.log('liked')
+    }
   return (
-    <Card>
+    <Card fluid>
       <Card.Content>
         <Image
           floated="right"
           size="mini"
-          src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+          src="https://i.pinimg.com/originals/17/2e/05/172e05d2a862ff36a5d17d3ed7c978d2.png"
         />
         <Card.Header>{username}</Card.Header>
-        <Card.Meta>{moment(createdAt).fromNow(true)}</Card.Meta>
-        <Card.Description>
-          {body}
-        </Card.Description>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <p>buttons</p>
+        <Button as="div" labelPosition="right" onClick={likePost}>
+          <Button color="orange" basic>
+            <Icon name="heart" />
+          </Button>
+          <Label basic color="orange" pointing="left">
+            {likeCount}
+          </Label>
+        </Button>
       </Card.Content>
     </Card>
   );
