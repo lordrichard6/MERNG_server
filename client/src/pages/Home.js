fe7370fea4d3621cf9f2d0048767506e13334e32
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-13 10:42:21 +0100

    setup to show posts in home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
 
 function Home() {
   const {
@@ -8,14 +9,24 @@ function Home() {
     data: { getPosts: post },
   } = useQuery(FETCH_POSTS_QUERY);
 
-  if (data) {
-    console.log(data);
-  }
-
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row>
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading Posts...</h1>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   );
 }
 
