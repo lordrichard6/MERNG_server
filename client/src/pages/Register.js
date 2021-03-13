4d57a51ecd0f8e8bd5094de2f13b0c3b3b796305
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-13 13:15:52 +0100

    setup registration page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+import gql from "graphql-tag";
 
 function Register() {
+  const [values, setvalues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const onChange = (e) => {
+    setvalues({ ...values, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
-      <h1>Register page</h1>
+      <Form onSubmit={onSubmit} noValidate>
+        <h1>Register</h1>
+        <Form.Input
+          label="Username"
+          palceholder="Username..."
+          name="username"
+          value={values.username}
+          onChange={onChange}
+        />
+        <Form.Input
+          label="Email"
+          palceholder="Email..."
+          name="email"
+          value={values.email}
+          onChange={onChange}
+        />
+        <Form.Input
+          label="Password"
+          palceholder="Password..."
+          name="password"
+          value={values.password}
+          onChange={onChange}
+        />
+        <Form.Input
+          label="Confirm Password"
+          palceholder="Confirm Password..."
+          name="confirmPassword"
+          value={values.confirmPassword}
+          onChange={onChange}
+        />
+        <Button type="submit" primary>
+          Register
+        </Button>
+      </Form>
     </div>
   );
 }
+
+
 
 export default Register;
