30123b358d2c52990a99e0a830efcd498460be78
lordrichard6 <paulolopesreizinho@gmail.com>
2021-03-13 19:04:26 +0100

    change navbar with context

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Menu } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-function MenuBar() {
-  const pathname = window.location.pathname;
-  const path = pathname === "/" ? "home" : pathname.substr(1);
+import { AuthContext } from "../context/auth";
 
+function MenuBar() {
+  const { user, logout } = useContext(AuthContext);
+  const pathname = window.location.pathname;
+
+  const path = pathname === "/" ? "home" : pathname.substr(1);
   const [activeItem, setActiveItem] = useState(path);
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
-  return (
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="orange">
+      <Menu.Item name={user.username} active as={Link} to="/" />
+      <Menu.Menu position="right">
+        <Menu.Item name="logout" onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
     <Menu pointing secondary size="massive" color="orange">
       <Menu.Item
         name="home"
@@ -19,7 +29,6 @@ function MenuBar() {
         as={Link}
         to="/"
       />
-
       <Menu.Menu position="right">
         <Menu.Item
           name="login"
@@ -38,6 +47,8 @@ function MenuBar() {
       </Menu.Menu>
     </Menu>
   );
+
+  return menuBar;
 }
 
 export default MenuBar;
